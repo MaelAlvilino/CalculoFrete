6e045d1f4dde1cd5f2f0009796151e70be4f41b5
MaelAlvilino <emmanuel.alvi@hotmail.com>
2024-03-27 11:09:48 -0300

    feat: initial

---
+++ src/main/java/github/CalcularFrete/CalcularFreteApplication.java
@@ -0,0 +1,13 @@
+package github.CalcularFrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcularFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcularFreteApplication.class, args);
+	}
+
+}
